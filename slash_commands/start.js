431d6fb504addce8defa464454d6015e4a0de7cc
Lilian Couchet <lilian@couchet.org>
2026-01-15 17:21:26 +0100

    Update start message

--- slash_commands/start.js
+++ slash_commands/start.js
@@ -1,6 +1,6 @@
-import {ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder} from "discord.js";
+import {ChatInputCommandInteraction, EmbedBuilder, MessageFlags, SlashCommandBuilder} from "discord.js";
 import {Player} from "../database/models/Player";
-
+import {welcomeText} from "../config/misc.json"
 
 module.exports = {
     data: new SlashCommandBuilder().setName("start").setDescription("Start a new game"),
@@ -10,23 +10,28 @@ module.exports = {
      * @param {Player} player
      */
     async execute(interaction, player) {
+        // if (player) {
+        //     return interaction.reply({
+        //         content: "You have already started your adventure !",
+        //         flags:  MessageFlags.Ephemeral,});
+        // }
+        //
+        // const newPlayer = Player.build({
+        //     discordId: interaction.user.id,
+        //     lvl: 1,
+        //     xp: 0,
+        //     stamina: 100,
+        //     gold: 100000
+        // });
+        // await newPlayer.save();
 
-        if (player) {
-            return interaction.reply({
-                content: "You have already started your adventure !",
-                flags:  MessageFlags.Ephemeral,});
-        }
-
-        const newPlayer = Player.build({
-            discordId: interaction.user.id,
-            lvl: 1,
-            xp: 0,
-            stamina: 100,
-        });
-        await newPlayer.save();
-
+        const text = welcomeText.replace("[discord nickname]", `**${interaction.user.displayName}**`);
+        const embed = new EmbedBuilder()
+            .setTitle("Welcome !")
+            .setDescription(text)
+            .setColor("Green");
         await interaction.reply({
-            content: "You have started your adventure !",
+            embeds: [embed],
             flags:  MessageFlags.Ephemeral,});
 
     }

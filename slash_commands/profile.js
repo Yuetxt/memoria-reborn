e9f19a2c4fd0fcfb5bbc63a213bc44fa4176404e
Lilian Couchet <lilian@couchet.org>
2026-01-28 08:49:23 +0100

    Copy the rest of the old command

--- slash_commands/profile.js
+++ slash_commands/profile.js
@@ -6,6 +6,7 @@ const {getServantData} = require("../utils/battle/setup");
 const {staticUrl} = require("../config.json")
 const gachaConfig = require("../config/gacha.json")
 const {calculateXp} = require("../utils/xp");
+const {Op} = require("sequelize");
 
 
 module.exports = {
@@ -15,26 +16,52 @@ module.exports = {
      * @param {Player} player
      */
     async execute(interaction, player) {
-        console.log(player, player.xp)
         const expNeeded = calculateXp(player.lvl + 1, "player");
         const expProgress = Math.floor((player.xp / expNeeded) * 100);
 
 
         const embed = new EmbedBuilder()
             .setColor('#4ECDC4')
-            .setTitle(`${interaction.member.name || interaction.user.displayName}'s Profile`)
+            .setTitle(`${interaction.user.displayName}'s Profile`)
             .setThumbnail(interaction.user.displayAvatarURL())
             .addFields(
-                { name: '📊 Level', value: `Level ${player.lvl} (${player.getXpProgress().progress*100}%)`, inline: true },
-                // { name: '⚡ Stamina', value: `${player.stamina}/${player.maxStamina }`, inline: true }, // TODO : integrate stamina increase
+                { name: '📊 Level', value: `Level ${player.lvl} (${(player.getXpProgress().progress*100).toFixed(0)}%)`, inline: true },
+                { name: '⚡ Stamina', value: `${player.stamina}/${player.maxStamina }`, inline: true }, // TODO : integrate stamina increase
                 { name: '🏔️ Current Floor', value: player.floor, inline: true },
                 { name: '💰 Gold', value: player.gold.toLocaleString(), inline: true },
-                // { name: '💎 Gems', value: player.gems.toLocaleString(), inline: true },
-                // { name: '⚔️ Battles Won', value: player.totalBattlesWon.toString(), inline: true }
+                { name: '💎 Gems', value: player.gems.toLocaleString(), inline: true },
+                { name: '⚔️ Battles Won', value: "0", inline: true }
             );
 
+        const team = await Servant.findAll({
+            where: {
+                playerId: interaction.user.id,
+                teamSlot: {[Op.gt]: -1}
+            }
+        })
+        if (team.length > 0) {
+            const teamValue = team.map((s, i) => {
+                const data = getServantData(s.servant_id)
+                console.log("DATA", data)
+                return `**Slot ${i + 1}:** ${data.name} (Lv.${s.lvl} | Bond ${s.bondLvl})`
+                }
+            ).join('\n');
+            embed.addFields({ name: '👥 Current Team', value: teamValue });
+        }
+        const servants = await Servant.findAll({
+            where: {playerId: interaction.user.id},
+            attributes: ["rarity"]
+        })
+
+
+        embed.addFields({
+            name: '📚 Collection',
+            value: `Total Servants: ${servants.length}\n4★: ${servants.filter(s => s.rarity === 0).length} | 5★: ${servants.filter(s => s.rarity === 1).length} | 6★: ${servants.filter(s => s.rarity === 2).length}`
+        });
+        console.log("TEAM", team)
         await interaction.reply({
             embeds: [embed],
+            allowedMentions: {users: []}
         })
     }
 }
